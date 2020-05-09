e5f4fa804a05541107b7dfe78e2ececedc7c060a
Luke Walsh <kyon.rf@gmail.com>
2020-05-09 11:58:50 +0100

    Added a way to have an inline select like Polaris 3

--- src/components/PolarisSelect.vue
+++ src/components/PolarisSelect.vue
@@ -4,12 +4,12 @@
     :label="label"
     :error="error"
     :action="labelAction"
-    :label-hidden="labelHidden"
+    :label-hidden="(labelInline) ? true : labelHidden"
     :help-text="helpText">
     <template v-if="$slots.helpText" slot="helpText">
         <slot name="helpText"></slot>
     </template>
-    
+
     <div :class="classes">
         <select
             :id="realId"
@@ -22,17 +22,17 @@
             @change="onChange"
             :aria-invalid="!!error"
             :aria-describedby="describedBy">
-            <option 
+            <option
                 v-if="!value && placeholder"
                 :label="placeholder"
                 :value="null"
                 disabled
                 hidden>
             </option>
-            
+
             <template v-if="options">
                 <template v-for="option in options">
-                    <option 
+                    <option
                         v-if="typeof option == 'string'"
                         :key="option"
                         :value="option">
@@ -51,7 +51,7 @@
                 <template v-for="group in groups">
                     <template v-if="!group.hasOwnProperty('title')">
                         <template v-for="option in group">
-                            <option 
+                            <option
                                 v-if="typeof option == 'string'"
                                 :key="option"
                                 :value="option">
@@ -71,7 +71,7 @@
                             :label="group.title"
                             :key="group.title">
                             <template v-for="option in group.options">
-                                <option 
+                                <option
                                     v-if="typeof option == 'string'"
                                     :key="option"
                                     :value="option">
@@ -90,12 +90,15 @@
                 </template>
             </template>
         </select>
-        
-        <div class="Polaris-Select__Icon">
-            <polaris-icon :source="arrowUpDown">
-            </polaris-icon>
+
+        <div class="Polaris-Select__Content">
+            <span v-if="labelInline" class="Polaris-Select__InlineLabel">{{label}}</span>
+            <span class="Polaris-Select__SelectedOption">{{(!value && placeholder) ? placeholder : value}}</span>
+            <div class="Polaris-Select__Icon">
+                <polaris-icon :source="arrowUpDown"></polaris-icon>
+            </div>
         </div>
-        
+
         <div class="Polaris-Select__Backdrop"></div>
     </div>
 </polaris-labelled>
@@ -105,7 +108,6 @@
 <script>
 import PolarisLabelled from './PolarisLabelled.vue';
 import ComponentHelpers from '../ComponentHelpers.js';
-
 import arrowUpDown from '../resources/arrow-up-down.svg';
 
 export default {
@@ -142,6 +144,7 @@ export default {
         name: String,
         error: [Boolean, String],
         disabled: Boolean,
+        labelInline: Boolean,
         value: String,
         placeholder: String
     },
@@ -153,15 +156,15 @@ export default {
             var r = ComponentHelpers.makeComponentClass('Polaris-Select', [
                 'disabled',
             ], this);
-            
+
             if (this.error) {
                 r['Polaris-Select--error'] = true;
             }
-            
+
             if (this.value == null && this.placeholder != null) {
                 r['Polaris-Select--placeholder'] = true;
             }
-            
+
             return r;
         },
         describedBy() {
@@ -188,3 +191,64 @@ export default {
     }
 };
 </script>
+<style>
+
+    .Polaris-Select__Input {
+        font-size: 1.6rem;
+        font-weight: 400;
+        line-height: 2.4rem;
+        text-transform: none;
+        letter-spacing: normal;
+        position: absolute;
+        text-rendering: auto;
+        top: 0;
+        left: 0;
+        z-index: 30;
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        opacity: .001;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        color: #212b36 !important;
+    }
+
+    .Polaris-Select__Input option:disabled{
+        color: #919eab;
+    }
+
+    .Polaris-Select__InlineLabel {
+        color: #637381;
+        margin-right: .4rem;
+        white-space: nowrap;
+        overflow: hidden;
+    }
+
+    .Polaris-Select__Content {
+        font-size: 1.6rem;
+        font-weight: 400;
+        line-height: 2.4rem;
+        text-transform: none;
+        letter-spacing: normal;
+        position: relative;
+        z-index: 20;
+        display: flex;
+        align-items: center;
+        width: 100%;
+        min-height: 3.6rem;
+        padding: .5rem .8rem .5rem 1.2rem;
+    }
+
+    .Polaris-Select__SelectedOption {
+        flex: 1 1;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+
+    @media (min-width: 40em) {
+        .Polaris-Select__Content {
+            font-size: 1.4rem;
+        }
+    }
+</style>
