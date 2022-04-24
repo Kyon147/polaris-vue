37ba5cb8a8ddd7b0e45d260225d5c2122a1b9822
Luke Walsh <kyon.rf@gmail.com>
2022-04-24 16:06:59 +0100

    Add hoverable to DataTable

--- src/components/PolarisDataTable.vue
+++ src/components/PolarisDataTable.vue
@@ -8,7 +8,8 @@
                             :key="index"
                             :class="[
                                 index === 0 ? 'Polaris-DataTable__Cell--firstColumn' : '',
-                                cellContentTypeClass(index)
+                                cellContentTypeClass(index),
+                                cellAlignment()
                             ]"
                             class="Polaris-DataTable__Cell Polaris-DataTable__Cell--header"
                             scope="col"
@@ -16,13 +17,14 @@
                             {{ heading }}
                         </th>
                     </tr>
-                    <tr v-if="totals && totals.length > 0">
+                    <tr v-if="totals && totals.length > 0 && !showTotalsInFooter">
                         <th v-for="(total, index) in totals"
                             :key="index"
                             :class="[
                                 'Polaris-DataTable__Cell',
                                 'Polaris-DataTable__Cell--total',
-                                cellContentTypeClass(index)
+                                cellContentTypeClass(index),
+                                cellAlignment()
                             ]">
                             {{ index === 0 ? 'Totals' : total }}
                         </th>
@@ -31,12 +33,16 @@
                 <tbody>
                     <template v-if="rows && rows.length > 0">
                         <tr v-for="(row, index) in rows" :key="index"
-                        :class="['Polaris-DataTable__TableRow Polaris-DataTable--hoverable']"
+                        :class="[
+                            'Polaris-DataTable__TableRow',
+                            hoverable ? 'Polaris-DataTable--hoverable' : ''
+                        ]"
                         >
                             <template v-for="(rowData, dataIndex) in row">
                                 <th v-if="dataIndex === 0"
                                     :class="[
                                         'Polaris-DataTable__Cell',
+                                        cellAlignment(),
                                         dataIndex === 0 ? 'Polaris-DataTable__Cell--firstColumn' : '',
                                          truncate ? 'Polaris-DataTable__Cell--truncated' : ''
                                     ]"
@@ -48,6 +54,7 @@
                                 <td v-else
                                     :class="[
                                         cellContentTypeClass(dataIndex),
+                                        cellAlignment(),
                                         truncate ? 'Polaris-DataTable__Cell--truncated' : ''
                                     ]"
                                     class="Polaris-DataTable__Cell"
@@ -58,6 +65,21 @@
                         </tr>
                     </template>
                 </tbody>
+                <tfoot v-if="showTotalsInFooter">
+                    <tr v-if="totals && totals.length > 0">
+                        <th v-for="(total, index) in totals"
+                            :key="index"
+                            :class="[
+                                'Polaris-DataTable__Cell',
+                                'Polaris-DataTable__Cell--total',
+                                'Polaris-DataTable--cellTotalFooter',
+                                cellContentTypeClass(index),
+                                cellAlignment()
+                            ]">
+                            {{ index === 0 ? 'Totals' : total }}
+                        </th>
+                    </tr>
+                </tfoot>
             </table>
         </div>
     </div>
@@ -90,13 +112,42 @@ export default {
         truncate:{
             type: Boolean,
             default: false
+        },
+        verticalAlign: {
+            type: String,
+            default: "middle",
+            validator(val){
+                return [
+                    'top',
+                    'middle',
+                    'bottom'
+                ].includes(val.toLowerCase())
+            }
+        },
+        hideScrollIndicator:{
+            type: Boolean,
+            default: false
+        },
+        showTotalsInFooter:{
+            type: Boolean,
+            default: false
+        },
+        hoverable:{
+            type: Boolean,
+            default: false
         }
     },
 
     methods: {
+        cellAlignment() {
+            return 'Polaris-DataTable__Cell--verticalAlign' + this.verticalAlign[0].toUpperCase() + this.verticalAlign.substring(1)
+        },
         cellContentTypeClass(index) {
             return this.columnContentTypes && this.columnContentTypes[index] ? 'Polaris-DataTable__Cell--' + this.columnContentTypes[index] : ''
-        }
+        },
+    },
+
+    computed: {
     }
 }
 </script>
@@ -105,6 +156,12 @@ export default {
 .Polaris-DataTable {
     position: relative;
     max-width: 100vw;
+
+    &--cellTotalFooter, &__Footer {
+        border-top: var(--p-border-divider);
+        border-bottom-left-radius: var(--p-border-radius-1);
+        border-bottom-right-radius: var(--p-border-radius-1);
+    }
 
     &__ScrollContainer {
         overflow-x: auto;
@@ -116,7 +173,7 @@ export default {
     }
 
     &__Cell {
-        padding: var(--p-space-4);
+        padding: var(--p-space-6);
         white-space: nowrap;
         text-align: left;
         transition: background-color .2s ease-in-out;
@@ -166,6 +223,10 @@ export default {
 
     .Polaris-DataTable__TableRow + .Polaris-DataTable__TableRow .Polaris-DataTable__Cell {
         border-top: var(--p-border-divider);
+    }
+
+    .Polaris-DataTable--hoverable:hover .Polaris-DataTable__Cell {
+        background: var(--p-surface-hovered);
     }
 }
 </style>
